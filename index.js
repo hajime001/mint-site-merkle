fb3772f99697d97433f410a36475c3f004b1aae7
はじめ <tomo2@juno.dti.ne.jp>
2023-02-20 17:53:06 +0900

    ethersのversionを「6.0.2」から「5.7.2」に変更 testを「npm run test」で動作するように修正

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ export class MintSiteMerkle {
         // types: ['address', 'uint256']
         // lists: [ ['0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 5], ['0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 5] ]
         for(let i = 0; i < lists.length; ++i) {
-            lists[i][0] = ethers.getAddress(lists[i][0]);
+            lists[i][0] = ethers.utils.getAddress(lists[i][0]);
         }
-        const leafNodes = lists.map(list => ethers.solidityPackedKeccak256(types, list));
+        const leafNodes = lists.map(list => ethers.utils.solidityKeccak256(types, list));
 
         this.types = types;
         this.lists = lists;
@@ -22,17 +22,17 @@ export class MintSiteMerkle {
     }
 
     verify(values) {
-        values[0] = ethers.getAddress(values[0]);
+        values[0] = ethers.utils.getAddress(values[0]);
 
-        return this.merkleTree.verify(this.getHexProof(values[0]), ethers.solidityPackedKeccak256(this.types, values), this.merkleTree.getRoot());
+        return this.merkleTree.verify(this.getHexProof(values[0]), ethers.utils.solidityKeccak256(this.types, values), this.merkleTree.getRoot());
     }
 
     getHexProof(address) {
-        const adjustAddress = ethers.getAddress(address);
+        const adjustAddress = ethers.utils.getAddress(address);
 
         for(let i = 0; i < this.lists.length; ++i) {
             if (this.lists[i][0] === adjustAddress) {
-                const claimingAddress = ethers.solidityPackedKeccak256(this.types, this.lists[i]);
+                const claimingAddress = ethers.utils.solidityKeccak256(this.types, this.lists[i]);
 
                 return this.merkleTree.getHexProof(claimingAddress);
             }
